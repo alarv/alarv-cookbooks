02558401d6905025d701c0efa0d45e13b51cb635
alarv <alarvfm@gmail.com>
2015-06-25 22:52:24 +0300

    added whenever recipe for test

---
+++ whenever/metadata.rb
@@ -0,0 +1,7 @@
+name             'whenever'
+maintainer       'Sapila'
+maintainer_email 'thesapila@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures whenever'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
